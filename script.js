ff95dd448bf55b30b7ffe3cab70a84d8c0823d7b
BC Ubuntu <balthazar.coquard@gmail.com>
2024-07-03 19:24:07 +0200

    Mouse effect, download feature, first links

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
 'use strict'
-document
-  .getElementsByClassName('btn-download')
-  .addEventListener('click', function () {
-    // File path
-    var filePath = '/assets/final-color-palet.png'
+document.addEventListener('DOMContentLoaded', (event) => {
+  document
+    .getElementById('btn-download')
+    .addEventListener('click', function () {
+      // Set file path
+      var filePath = '/assets/Coquard-Balthazar-CV_de.pdf'
 
-    // Creation anchor element with
-    var link = document.createElement('a')
-    link.href = filePath
-    link.download = 'file-to-download.pdf' // Default download file name
-    link.target = '_blank' // Open in new tab
+      // Creation anchor element
+      var link = document.createElement('a')
+      link.href = filePath
+      link.download = 'Coquard-Balthazar-CV_de.pdf' // Default download file name
+      link.target = '_blank' // Open in new tab
 
-    // Trigger the download
-    link.click()
+      // Trigger the download
+      link.click()
+    })
+})
+
+function addOpenLinkButtonListener(elementID, url) {
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById(elementID)
+    if (button) {
+      button.addEventListener('click', function () {
+        window.open(url, '_blank')
+      })
+    } else {
+      console.error(`Element with ID ${elementID} not found.`)
+    }
   })
+}
 
-/* navigation til lets talk section
-document,
-  getElementsByClassName('btn-lets-talk').addEventListener(
-    'click',
-    function () {},
-  )
-*/
+addOpenLinkButtonListener(
+  'link-python-conterter-ofx',
+  'https://github.com/BalthazarCoq/ofxToCSV',
+)
+addOpenLinkButtonListener(
+  'link-js-pig-game',
+  'https://github.com/BalthazarCoq/pigGame-js',
+)
+addOpenLinkButtonListener(
+  'link-mspa-approval-request',
+  'https://github.com/BalthazarCoq/mspa-approval-request-apps',
+)
+addOpenLinkButtonListener(
+  'link-js-guess-my-number',
+  'https://github.com/BalthazarCoq/guessMyNumber-js',
+)
+addOpenLinkButtonListener(
+  'link-python-blackjack',
+  'https://github.com/BalthazarCoq/game-blackjack',
+)
+addOpenLinkButtonListener(
+  'link-python-blackjack',
+  'https://github.com/BalthazarCoq/game-blackjack',
+)
+
+href = '#projects'
